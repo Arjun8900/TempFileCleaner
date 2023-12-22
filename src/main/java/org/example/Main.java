0f4db05d1340f472347f2295efe91af3b8f30c70
Arjun Singh Kanwal <kanwal.arjun8900@gmail.com>
2023-12-22 09:25:18 +0530

    Updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         try {
-            File file = new File("abc.txt");
+            File file = new File("Input.txt");
             List<String> input = Files.readAllLines(file.toPath());
             System.out.println(input);
         } catch (IOException e) {
