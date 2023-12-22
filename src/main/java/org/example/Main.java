8264b18dbfb68c58718279289e97daa208420e28
Arjun Singh Kanwal <kanwal.arjun8900@gmail.com>
2023-12-22 09:21:56 +0530

    Updated

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            File file = new File("abc.txt");
+            List<String> input = Files.readAllLines(file.toPath());
+            System.out.println(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
