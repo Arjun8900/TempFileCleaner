d3be15085420bb418aed0d427a051e1c65a96ceb
Arjun Singh Kanwal <kanwal.arjun8900@gmail.com>
2023-12-23 09:38:14 +0530

    Added FInal

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+java -cp "C:\Users\kanwa\Documents\CODE\TempFileCleaner\target\TempFileCleaner-1.0-SNAPSHOT.jar" "org.example.Main" Input.txt
+pause
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,55 @@
 package org.example;
 
+
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Please provide input file containing the temp folder list");
+            return;
+        }
+        String inputFile = args[0];
         try {
-            File file = new File("Input.txt");
-            List<String> input = Files.readAllLines(file.toPath());
-            System.out.println(input);
+            File input = new File(inputFile);
+            List<String> folders = Files.readAllLines(input.toPath());
+            System.out.println(folders);
+
+            for (String folder: folders) {
+                File file = new File(folder);
+                File[] contents = file.listFiles();
+                int deletedCount = 0;
+                int totalCount = contents != null ? contents.length : 0;
+                try {
+                    if (contents != null) {
+                        for (File content : contents) {
+                            if (deleteDir(content)) {
+                                deletedCount++;
+                            }
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    System.out.println("Error in folder = " + folder);
+                    e.printStackTrace();
+                }
+                System.out.printf("Deleted files from inside Folder = %s, Deleted = %s/%s, Remaining = %s %n", folder, deletedCount, totalCount, totalCount - deletedCount);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static boolean deleteDir(File file) {
+        File[] contents = file.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                deleteDir(f);
+            }
+        }
+        return file.delete();
     }
 }
